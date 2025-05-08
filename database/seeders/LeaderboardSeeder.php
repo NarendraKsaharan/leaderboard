500d2a65a7b6ed1f76bb65b802f6364402bec820
KumarNarendra99 <narendrasaharan97@gmail.com>
2025-05-08 19:07:46 +0530

    intial commit

--- database/seeders/LeaderboardSeeder.php
+++ database/seeders/LeaderboardSeeder.php
@@ -14,16 +14,6 @@ class LeaderboardSeeder extends Seeder
      */
     public function run(): void
     {
-        // $users = User::with('activities')->get();
-        // foreach ($users as $user) {
-        //     $user->total_points = $user->activities->sum('points');
-        // }
-        
-        // $sorted = $users->sortByDesc('total_points')->values();
-        
-        // $prevPoints = null;
-        // $rank = 0;
-        
         $users = User::with('activities')->get()
             ->map(function ($user) {
                 $user->total_points = $user->activities->sum('points');
